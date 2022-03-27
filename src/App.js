b7e2c0dd6acc43626d4662cb83b7a8d5ad0e5547
Michael Fatemi <myfatemi04@gmail.com>
2022-03-27 03:34:57 -0400

    create pannable dashboard

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <div
         style={{
-          width: "80rem",
+          width: "80vw",
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
@@ -22,22 +22,24 @@ function App() {
         >
           <form
             method="post"
-            action="http://127.0.0.1:5000/detect"
+            action="http://127.0.0.1:5000/submit_photo"
             encType="multipart/form-data"
+            style={{
+              display: "flex",
+              alignItems: "center",
+            }}
           >
             <input type="file" name="file" className="hover-btn" />
-            <button type="submit" className="hover-btn">
+            <input name="lat" type="hidden" value="37.7749" />
+            <input name="lon" type="hidden" value="-122.4194" />
+            <button
+              type="submit"
+              className="hover-btn"
+              style={{ height: "100%" }}
+            >
               Upload
             </button>
           </form>
-          <div
-            className="hover-btn"
-            style={{
-              marginLeft: "10px",
-            }}
-          >
-            View dashboard
-          </div>
         </div>
         <Dashboard />
       </div>
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,5 +1,5 @@
 import GoogleMapReact from "google-map-react";
-import { useState } from "react";
+import { useEffect, useRef, useState, useMemo } from "react";
 
 function generatePositions() {
   const positions = [];
@@ -14,37 +14,95 @@ function generatePositions() {
 
 const positions = generatePositions();
 
-//https://www.npmjs.com/package/google-map-react
-//https://zjor.medium.com/heatmaps-with-google-map-react-57e279315060
+// https://www.npmjs.com/package/google-map-react
+// https://zjor.medium.com/heatmaps-with-google-map-react-57e279315060
 export default function Dashboard() {
-  const [map, setMap] = useState();
+  const mapRef = useRef();
   const center = { lat: 37.7749, lng: -122.4194 };
   const zoom = 9;
-  return (
-    <div
-      style={{
-        height: "60vh",
-        width: "100%",
+  const [northEast, setNorthEast] = useState();
+  const [southWest, setSouthWest] = useState();
 
-        borderRadius: "1rem",
-        overflow: "hidden",
-      }}
-    >
-      <GoogleMapReact
-        ref={setMap}
-        bootstrapURLKeys={{ key: "AIzaSyDUnWIrt-H4RuP2YFLpVPz4oAjBhpOOoyI" }}
-        defaultCenter={center}
-        defaultZoom={zoom}
-        heatmapLibrary={true}
-        heatmap={{
-          positions,
-          options: {
-            radius: 20,
-            opacity: 0.7,
-          },
+  useEffect(() => {
+    if (!mapRef.current) return;
+
+    const map = mapRef.current.map_;
+
+    if (!map) {
+      return;
+    }
+
+    let previousCallTime = 0;
+
+    const mapBoundsChangedListener = () => {
+      const currentCallTime = new Date().getTime();
+
+      if (currentCallTime - previousCallTime < 200) {
+        return;
+      }
+
+      const bounds = map.getBounds();
+      const northEast = bounds.getNorthEast();
+      const southWest = bounds.getSouthWest();
+
+      setNorthEast(northEast.toJSON());
+      setSouthWest(southWest.toJSON());
+
+      previousCallTime = currentCallTime;
+    };
+
+    map.addListener("center_changed", mapBoundsChangedListener);
+    map.addListener("zoom_changed", mapBoundsChangedListener);
+
+    return () => {
+      map.removeListener("center_changed", mapBoundsChangedListener);
+      map.removeListener("zoom_changed", mapBoundsChangedListener);
+    };
+  }, [mapRef]);
+
+  const visiblePositions = useMemo(() => {
+    if (!northEast || !southWest) {
+      return positions;
+    }
+
+    return positions.filter(
+      (position) =>
+        position.lat > southWest.lat &&
+        position.lat < northEast.lat &&
+        position.lng > southWest.lng &&
+        position.lng < northEast.lng
+    );
+  }, [northEast, southWest]);
+
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <div
+        style={{
+          height: "60vh",
+          width: "60vw",
+          minWidth: "30rem",
+
+          borderRadius: "1rem",
+          overflow: "hidden",
         }}
-        onClick={() => {}}
-      ></GoogleMapReact>
+      >
+        <GoogleMapReact
+          ref={mapRef}
+          bootstrapURLKeys={{ key: "AIzaSyDUnWIrt-H4RuP2YFLpVPz4oAjBhpOOoyI" }}
+          defaultCenter={center}
+          defaultZoom={zoom}
+          heatmapLibrary={true}
+          heatmap={{
+            positions,
+            options: {
+              radius: 20,
+              opacity: 0.7,
+            },
+          }}
+          onClick={() => {}}
+        />
+      </div>
+      <p>{visiblePositions.length} instances of garbage found in this area.</p>
     </div>
   );
 }
